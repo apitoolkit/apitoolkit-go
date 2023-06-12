5a08a26919abbd1edb410fb3bcefd06e0b04e400
Anthony Alaribe <anthonyalaribe@gmail.com>
2023-06-12 18:51:16 +0200

    improve testing coverage

---
+++ gin_test.go
@@ -0,0 +1,149 @@
+package apitoolkit
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/imroc/req"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGinMiddleware(t *testing.T) {
+	client := &Client{
+		config: &Config{
+			RedactHeaders:      []string{"X-Api-Key", "Accept-Encoding"},
+			RedactResponseBody: exampleDataRedaction, 
+		},
+	}
+	var publishCalled bool
+
+	client.PublishMessage = func(ctx context.Context, payload Payload) error {
+		assert.Equal(t, "POST", payload.Method)
+		assert.Equal(t, "/:slug/test", payload.URLPath)
+		assert.Equal(t, map[string]string{
+			"slug": "slug-value",
+		}, payload.PathParams)
+		assert.Equal(t, map[string][]string{
+			"param1": {"abc"},
+			"param2": {"123"},
+		}, payload.QueryParams)
+
+		assert.Equal(t, map[string][]string{
+			"Accept-Encoding": {"gzip"},
+			"Content-Length":  {"437"},
+			"Content-Type":    {"application/json"},
+			"User-Agent":      {"Go-http-client/1.1"},
+			"X-Api-Key":       {"past-3"},
+		}, payload.RequestHeaders)
+		assert.Equal(t, map[string][]string{
+			"Content-Type": {"application/json"},
+			"X-Api-Key":    {"applicationKey"},
+		}, payload.ResponseHeaders)
+		assert.Equal(t, "/slug-value/test?param1=abc&param2=123", payload.RawURL)
+		assert.Equal(t, http.StatusAccepted, payload.StatusCode)
+		assert.Greater(t, payload.Duration, 1000*time.Nanosecond)
+		assert.Equal(t, GoGinSDKType, payload.SdkType)
+
+		reqData, _ := json.Marshal(exampleData2)
+		respData, _ := json.Marshal(exampleDataRedacted)
+
+		assert.Equal(t, reqData, payload.RequestBody)
+		assert.Equal(t, respData, payload.ResponseBody)
+
+		publishCalled = true
+		return nil
+	}
+
+	router := gin.New()
+	router.Use(client.GinMiddleware)
+	router.POST("/:slug/test", func(c *gin.Context) {
+		body, err := ioutil.ReadAll(c.Request.Body)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, body)
+		reqData, _ := json.Marshal(exampleData2)
+		assert.Equal(t, reqData, body)
+		c.Header("Content-Type", "application/json")
+		c.Header("X-API-KEY", "applicationKey")
+		c.JSON(http.StatusAccepted, exampleData)
+	})
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	respData, _ := json.Marshal(exampleData)
+	resp, err := req.Post(ts.URL+"/slug-value/test",
+		req.Param{"param1": "abc", "param2": 123},
+		req.Header{
+			"Content-Type": "application/json",
+			"X-API-KEY":    "past-3",
+		},
+		req.BodyJSON(exampleData2),
+	)
+	assert.NoError(t, err)
+	assert.True(t, publishCalled)
+	assert.Equal(t, respData, resp.Bytes())
+}
+
+func TestGinMiddlewareGET(t *testing.T) {
+	client := &Client{
+		config: &Config{},
+	}
+	var publishCalled bool
+	respData, _ := json.Marshal(exampleData)
+	client.PublishMessage = func(ctx context.Context, payload Payload) error {
+		assert.Equal(t, "GET", payload.Method)
+		assert.Equal(t, "/:slug/test", payload.URLPath)
+		assert.Equal(t, map[string]string{
+			"slug": "slug-value",
+		}, payload.PathParams)
+		assert.Equal(t, map[string][]string{
+			"param1": {"abc"},
+			"param2": {"123"},
+		}, payload.QueryParams)
+		assert.Equal(t, map[string][]string{
+			"Accept-Encoding": {"gzip"},
+			"User-Agent":      {"Go-http-client/1.1"},
+			"X-Api-Key":       {"past-3"},
+		}, payload.RequestHeaders)
+		assert.Equal(t, map[string][]string{
+			"Content-Type": {"application/json"},
+		}, payload.ResponseHeaders)
+		assert.Equal(t, "/slug-value/test?param1=abc&param2=123", payload.RawURL)
+		assert.Equal(t, http.StatusAccepted, payload.StatusCode)
+		assert.Greater(t, payload.Duration, 1000*time.Nanosecond)
+		assert.Equal(t, []byte{0x6e, 0x75, 0x6c, 0x6c}, payload.RequestBody)
+		assert.Equal(t, respData, payload.ResponseBody)
+		assert.Equal(t, GoGinSDKType, payload.SdkType)
+		publishCalled = true
+		return nil
+	}
+	router := gin.New()
+	router.Use(client.GinMiddleware)
+	router.GET("/:slug/test", func(c *gin.Context) {
+		body, err := ioutil.ReadAll(c.Request.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, []byte{}, body)
+
+		c.Header("Content-Type", "application/json")
+		c.JSON(http.StatusAccepted, exampleData)
+	})
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	resp, err := req.Get(ts.URL+"/slug-value/test",
+		req.QueryParam{"param1": "abc", "param2": 123},
+		req.Header{
+			"X-API-KEY": "past-3",
+		},
+	)
+	assert.NoError(t, err)
+	assert.True(t, publishCalled)
+	assert.Equal(t, respData, resp.Bytes())
+}
--- sdk_test.go
+++ sdk_test.go
@@ -3,17 +3,13 @@ package apitoolkit
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 
-	"github.com/gin-gonic/gin"
 	"github.com/imroc/req"
-	"github.com/joho/godotenv"
-	"github.com/kr/pretty"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,22 +18,25 @@ func TestMain(m *testing.M) {
 }
 
 func TestAPIToolkitWorkflow(t *testing.T) {
-	_ = godotenv.Load(".env")
-	client, err := NewClient(context.Background(), Config{RootURL: "http://localhost:8080", APIKey: "x/ZLLsxMNSozzNcf1aZsSzhP9DiURoeev7rlgOhcq20C9t7D"})
+	// _ = godotenv.Load(".env")
+	// client, err := NewClient(context.Background(), Config{RootURL: "http://localhost:8080", APIKey: "x/ZLLsxMNSozzNcf1aZsSzhP9DiURoeev7rlgOhcq20C9t7D"})
 	// client, err := NewClient(context.Background(), Config{APIKey: "waAaLZEdNSkzlYdM0aZsTTYc9DmTSoCeuO3s0O0KoDBV9o/D"}) // prod test
-	if !assert.NoError(t, err) {
-		t.Fail()
-		return
+	// if !assert.NoError(t, err) {
+	// 	t.Fail()
+	// 	return
+	// }
+	// defer client.Close()
+	var err error
+	client := &Client{
+		config: &Config{},
 	}
-	defer client.Close()
 
 	t.Run("test golang native server middleware", func(t *testing.T) {
 		var publishCalled bool
-		// client.PublishMessage = func(ctx context.Context, payload Payload) error {
-		// 	publishCalled = true
-		// 	pretty.Println("payload", payload)
-		// 	return nil
-		// }
+		client.PublishMessage = func(ctx context.Context, payload Payload) error {
+			publishCalled = true
+			return nil
+		}
 
 		handlerFn := func(w http.ResponseWriter, r *http.Request) {
 			body, err := ioutil.ReadAll(r.Body)
@@ -67,135 +66,6 @@ func TestAPIToolkitWorkflow(t *testing.T) {
 		assert.NoError(t, err)
 		assert.True(t, publishCalled)
 	})
-	t.Run("test gin server middleware", func(t *testing.T) {
-		var publishCalled bool
-		// client.PublishMessage = func(ctx context.Context, payload Payload) error {
-		// 	publishCalled = true
-		// 	pretty.Println("payload", payload)
-		// 	return nil
-		// }
-
-		router := gin.New()
-		router.Use(client.GinMiddleware)
-		router.POST("/:slug/test", func(c *gin.Context) {
-			body, err := ioutil.ReadAll(c.Request.Body)
-			assert.NoError(t, err)
-			assert.NotEmpty(t, body)
-
-			c.Header("Content-Type", "application/json")
-			c.Header("X-API-KEY", "applicationKey")
-			c.JSON(http.StatusAccepted, exampleData)
-		})
-
-		router.NoRoute(func(c *gin.Context) {
-			fmt.Println("NO ROUTE HANDLER")
-		})
-
-		ts := httptest.NewServer(router)
-		defer ts.Close()
-
-		resp, err := req.Post(ts.URL+"/slug-value/test",
-			req.Param{"param1": "abc", "param2": 123},
-			req.Header{
-				"Content-Type": "application/json",
-				"X-API-KEY":    "past-3",
-			},
-			req.BodyJSON(exampleData2),
-		)
-		assert.NoError(t, err)
-		assert.True(t, publishCalled)
-		pretty.Println(resp.Dump())
-	})
-	t.Run("test gin server middleware with GET request", func(t *testing.T) {
-		var publishCalled bool
-		// client.PublishMessage = func(ctx context.Context, payload Payload) error {
-		// 	publishCalled = true
-		// 	pretty.Println("payload", payload)
-		// 	return nil
-		// }
-		client.PublishMessage = client.publishMessage
-
-		router := gin.New()
-		router.Use(client.GinMiddleware)
-		router.GET("/:slug/test", func(c *gin.Context) {
-			body, err := ioutil.ReadAll(c.Request.Body)
-			assert.NoError(t, err)
-			fmt.Println(string(body))
-
-			c.Header("Content-Type", "application/json")
-			c.Header("X-API-KEY", "applicationKey")
-			c.JSON(http.StatusAccepted, exampleData)
-		})
-
-		router.NoRoute(func(c *gin.Context) {
-			fmt.Println("NO ROUTE HANDLER")
-		})
-
-		ts := httptest.NewServer(router)
-		defer ts.Close()
-
-		resp, err := req.Get(ts.URL+"/slug-value/test",
-			req.QueryParam{"param1": "abc", "param2": 123},
-			req.Header{
-				"Content-Type": "application/json",
-				"X-API-KEY":    "past-3",
-			},
-		)
-		assert.NoError(t, err)
-		assert.True(t, publishCalled)
-		pretty.Println(resp.Dump())
-	})
-}
-
-func TestRedacting(t *testing.T) {
-	cfg := Config{
-		Debug: true,
-	}
-	client := Client{
-		config: &cfg,
-	}
-
-	var publishCalled bool
-	client.PublishMessage = func(ctx context.Context, payload Payload) error {
-		publishCalled = true
-		pretty.Println("🚀  payload", payload)
-		pretty.Println("🚀  RequestBody:", string(payload.RequestBody))
-		pretty.Println("🚀  ResponseBody:", string(payload.ResponseBody))
-
-		return nil
-	}
-
-	router := gin.New()
-	router.Use(client.GinMiddleware)
-	router.GET("/:slug/test", func(c *gin.Context) {
-		body, err := ioutil.ReadAll(c.Request.Body)
-		assert.NoError(t, err)
-		fmt.Println(string(body))
-
-		c.Header("Content-Type", "application/json")
-		c.Header("X-API-KEY", "applicationKey")
-		c.JSON(http.StatusAccepted, exampleData)
-	})
-
-	router.NoRoute(func(c *gin.Context) {
-		fmt.Println("NO ROUTE HANDLER")
-	})
-
-	ts := httptest.NewServer(router)
-	defer ts.Close()
-
-	resp, err := req.Get(ts.URL+"/slug-value/test",
-		req.QueryParam{"param1": "abc", "param2": 123},
-		req.Header{
-			"Content-Type":   "application/json",
-			"X-API-KEY":      "past-3",
-			"X-INPUT-HEADER": "testing",
-		},
-		req.BodyJSON(exampleData),
-	)
-	assert.NoError(t, err)
-	assert.True(t, publishCalled)
-	pretty.Println(resp.Dump())
 }
 
 func TestRedactFunc(t *testing.T) {
@@ -204,17 +74,16 @@ func TestRedactFunc(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		res := redact(exampleJSON, []string{"$.status", "$.data.account_data.possible_account_types", "$.non_existent"})
-		expected := `{"data":{"account_data":{"account_balance":"100.00","account_created_at":"2020-01-01T00:00:00Z","account_currency":"USD","account_deleted_at":"2020-01-01T00:00:00Z","account_id":"123456789","account_name":"test account","account_status":"active","account_type":"test","account_updated_at":"2020-01-01T00:00:00Z","batch_number":12345,"possible_account_types":"[CLIENT_REDACTED]"},"message":"hello world"},"status":"[CLIENT_REDACTED]"}`
-		assert.JSONEq(t, expected, string(res))
+		res := redact(exampleJSON, exampleDataRedaction)
+		expected, _ := json.Marshal(exampleDataRedacted)
+		assert.JSONEq(t, string(expected), string(res))
 	})
 
 	t.Run("redactHeaders", func(t *testing.T) {
-
 		result := redactHeaders(map[string][]string{
-			"Content-Type": []string{"application/json"},
-			"X-API-KEY":    []string{"test"},
-			"X-rando":      []string{"test 2"},
+			"Content-Type": {"application/json"},
+			"X-API-KEY":    {"test"},
+			"X-rando":      {"test 2"},
 		}, []string{"Content-Type", "X-rando"})
 		assert.Equal(t, result, map[string][]string{
 			"Content-Type": {"[CLIENT_REDACTED]"},
@@ -229,17 +98,46 @@ var exampleData = map[string]interface{}{
 	"data": map[string]interface{}{
 		"message": "hello world",
 		"account_data": map[string]interface{}{
-			"batch_number":           12345,
-			"account_id":             "123456789",
-			"account_name":           "test account",
-			"account_type":           "test",
-			"account_status":         "active",
-			"account_balance":        "100.00",
-			"account_currency":       "USD",
-			"account_created_at":     "2020-01-01T00:00:00Z",
-			"account_updated_at":     "2020-01-01T00:00:00Z",
-			"account_deleted_at":     "2020-01-01T00:00:00Z",
-			"possible_account_types": []string{"test", "staging", "production"},
+			"batch_number":            12345,
+			"account_id":              "123456789",
+			"account_name":            "test account",
+			"account_type":            "test",
+			"account_status":          "active",
+			"account_balance":         "100.00",
+			"account_currency":        "USD",
+			"account_created_at":      "2020-01-01T00:00:00Z",
+			"account_updated_at":      "2020-01-01T00:00:00Z",
+			"account_deleted_at":      "2020-01-01T00:00:00Z",
+			"possible_account_types":  []string{"test", "staging", "production"},
+			"possible_account_types2": []string{"test", "staging", "production"},
+		},
+	},
+}
+
+var exampleDataRedaction = []string{
+	"$.status", "$.data.account_data.account_type",
+	"$.data.account_data.possible_account_types",
+	"$.data.account_data.possible_account_types2[*]",
+	"$.non_existent",
+}
+
+var exampleDataRedacted = map[string]interface{}{
+	"status": "[CLIENT_REDACTED]",
+	"data": map[string]interface{}{
+		"message": "hello world",
+		"account_data": map[string]interface{}{
+			"batch_number":            12345,
+			"account_id":              "123456789",
+			"account_name":            "test account",
+			"account_type":            "[CLIENT_REDACTED]",
+			"account_status":          "active",
+			"account_balance":         "100.00",
+			"account_currency":        "USD",
+			"account_created_at":      "2020-01-01T00:00:00Z",
+			"account_updated_at":      "2020-01-01T00:00:00Z",
+			"account_deleted_at":      "2020-01-01T00:00:00Z",
+			"possible_account_types":  "[CLIENT_REDACTED]",
+			"possible_account_types2": []string{"[CLIENT_REDACTED]", "[CLIENT_REDACTED]", "[CLIENT_REDACTED]"},
 		},
 	},
 }

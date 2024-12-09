e700879d51e0385f795b570158b0fbaa6e17d1f2
dawkaka <yousiph77@gmail.com>
2024-12-09 11:48:47 +0000

    feat: otel config function

--- go.mod
+++ go.mod
@@ -7,25 +7,37 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-errors/errors v1.5.1
 	github.com/valyala/fasthttp v1.51.0
+	go.opentelemetry.io/contrib/instrumentation/host v0.57.0
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.57.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.32.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.32.0
+	go.opentelemetry.io/otel/sdk/metric v1.32.0
+	google.golang.org/grpc v1.67.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/ebitengine/purego v0.8.1 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.23.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/lufia/plan9stats v0.0.0-20240909124753-873cd0166683 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.24.10 // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.32.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20241104194629-dd2ea8efbc28 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241104194629-dd2ea8efbc28 // indirect
-	google.golang.org/grpc v1.67.1 // indirect
 )
 
 require (
@@ -45,6 +57,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/labstack/echo/v4 v4.13.0
@@ -53,13 +66,14 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/sethvargo/go-envconfig v1.1.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	go.opentelemetry.io/otel v1.32.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.32.0
 	go.opentelemetry.io/otel/metric v1.32.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.32.0
-	go.opentelemetry.io/otel/trace v1.32.0
+	go.opentelemetry.io/otel/trace v1.32.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
---
+++ otelConfig/otelConfig.go
@@ -0,0 +1,327 @@
+package otelConfig
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/sethvargo/go-envconfig"
+	hostMetrics "go.opentelemetry.io/contrib/instrumentation/host"
+	runtimeMetrics "go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"google.golang.org/grpc/encoding/gzip"
+)
+
+const APITOOLKIT_ENDPOINT = "otelcol.apitoolkit.io:4317"
+
+type Option func(*Config)
+
+type Config struct {
+	TracesEnabled            *bool             `env:"OTEL_TRACES_ENABLED,default=true"`
+	ExporterEndpoint         string            `env:"OTEL_EXPORTER_OTLP_ENDPOINT,overwrite"`
+	ExporterEndpointInsecure bool              `env:"OTEL_EXPORTER_OTLP_INSECURE"`
+	ServiceName              string            `env:"OTEL_SERVICE_NAME,overwrite"`
+	ServiceVersion           string            `env:"OTEL_SERVICE_VERSION,overwrite,default=unknown"`
+	MetricsEnabled           *bool             `env:"OTEL_METRICS_ENABLED,default=true"`
+	MetricsReportingPeriod   string            `env:"OTEL_EXPORTER_OTLP_METRICS_PERIOD,overwrite,default=30s"`
+	LogLevel                 string            `env:"OTEL_LOG_LEVEL,overwrite,default=info"`
+	Propagators              []string          `env:"OTEL_PROPAGATORS,overwrite,default=tracecontext,baggage"`
+	ResourceAttributes       map[string]string `env:"OTEL_RESOURCE_ATTRIBUTES,overwrite,separator=="`
+	SpanProcessors           []trace.SpanProcessor
+	Sampler                  trace.Sampler
+	ResourceOptions          []resource.Option
+	Resource                 *resource.Resource
+	errorHandler             otel.ErrorHandler
+}
+
+func ConfigureOpenTelemetry(opts ...Option) (func(), error) {
+	c, err := newConfig(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	if c.LogLevel == "debug" {
+		log.Println("debug logging enabled")
+		log.Println("configuration")
+		s, _ := json.MarshalIndent(c, "", "\t")
+		log.Println(string(s))
+	}
+
+	if c.errorHandler != nil {
+		otel.SetErrorHandler(c.errorHandler)
+	}
+
+	otelConfig := OtelConfig{
+		config: c,
+	}
+
+	type setupFunc func(*Config) (func() error, error)
+
+	for _, setup := range []setupFunc{setupTracing, setupMetrics} {
+		shutdown, err := setup(c)
+		if err != nil {
+			return otelConfig.Shutdown, fmt.Errorf("setup error: %w", err)
+		}
+		if shutdown != nil {
+			otelConfig.shutdownFuncs = append(otelConfig.shutdownFuncs, shutdown)
+		}
+	}
+	return otelConfig.Shutdown, nil
+}
+
+func setupTracing(c *Config) (func() error, error) {
+	var enabled bool
+	if c.TracesEnabled == nil {
+		enabled = true
+	} else {
+		enabled = *c.TracesEnabled
+	}
+	if !enabled {
+		log.Println("tracing is disabled by configuration: enabled set to false")
+		return nil, nil
+	}
+
+	opts := []trace.TracerProviderOption{
+		trace.WithResource(c.Resource),
+		trace.WithSampler(c.Sampler),
+	}
+	for _, sp := range c.SpanProcessors {
+		opts = append(opts, trace.WithSpanProcessor(sp))
+	}
+	spanExporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			otlptracegrpc.WithInsecure(),
+			otlptracegrpc.WithEndpoint(c.ExporterEndpoint),
+			otlptracegrpc.WithCompressor(gzip.Name),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create span exporter: %v", err)
+	}
+
+	bsp := trace.NewBatchSpanProcessor(spanExporter)
+	opts = append(opts, trace.WithSpanProcessor(bsp))
+
+	tp := trace.NewTracerProvider(opts...)
+	// if err = configurePropagators(c); err != nil {
+	// 	return nil, err
+	// }
+	otel.SetTracerProvider(tp)
+	return func() error {
+		_ = bsp.Shutdown(context.Background())
+		return spanExporter.Shutdown(context.Background())
+	}, nil
+}
+
+func setupMetrics(c *Config) (func() error, error) {
+	var enabled bool
+	if c.MetricsEnabled == nil {
+		enabled = true
+	} else {
+		enabled = *c.MetricsEnabled
+	}
+	if !enabled {
+		log.Println("metrics are disabled by configuration: enabled set to false")
+		return nil, nil
+	}
+	metricExporter, err := otlpmetricgrpc.New(
+		context.Background(),
+		otlpmetricgrpc.WithEndpoint(c.ExporterEndpoint),
+		otlpmetricgrpc.WithInsecure(),
+		otlpmetricgrpc.WithCompressor(gzip.Name),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create metric exporter: %v", err)
+	}
+
+	var readerOpts []metric.PeriodicReaderOption
+	if c.MetricsReportingPeriod != "" {
+		period, err := time.ParseDuration(c.MetricsReportingPeriod)
+		if err != nil {
+			return nil, fmt.Errorf("invalid metric reporting period: %v", err)
+		}
+		if period <= 0 {
+			return nil, fmt.Errorf("invalid metric reporting period: %v", c.MetricsReportingPeriod)
+		}
+		readerOpts = append(readerOpts, metric.WithInterval(period))
+	}
+
+	meterProvider := metric.NewMeterProvider(
+		metric.WithResource(c.Resource),
+		metric.WithReader(metric.NewPeriodicReader(metricExporter, readerOpts...)))
+
+	if err = runtimeMetrics.Start(runtimeMetrics.WithMeterProvider(meterProvider)); err != nil {
+		return nil, fmt.Errorf("failed to start runtime metrics: %v", err)
+	}
+
+	if err = hostMetrics.Start(hostMetrics.WithMeterProvider(meterProvider)); err != nil {
+		return nil, fmt.Errorf("failed to start host metrics: %v", err)
+	}
+
+	otel.SetMeterProvider(meterProvider)
+	return func() error {
+		return meterProvider.Shutdown(context.Background())
+	}, nil
+}
+
+func newConfig(opts ...Option) (*Config, error) {
+	c := &Config{
+		ResourceAttributes: map[string]string{},
+		Sampler:            trace.AlwaysSample(),
+		ExporterEndpoint:   APITOOLKIT_ENDPOINT,
+	}
+
+	// apply environment variables last to override any vendor or user options
+	envError := envconfig.Process(context.Background(), c)
+	if envError != nil {
+		log.Fatalf("environment error: %v", envError)
+		return nil, fmt.Errorf("environment error: %w", envError)
+	}
+	for _, opt := range opts {
+		opt(c)
+	}
+	var err error
+	c.Resource, err = newResource(c)
+
+	if err != nil {
+		if errors.Is(err, resource.ErrSchemaURLConflict) {
+			log.Printf("schema conflict %v", err)
+			// ignore schema conflicts
+			err = nil
+		}
+	}
+
+	return c, err
+}
+
+type OtelConfig struct {
+	config        *Config
+	shutdownFuncs []func() error
+}
+
+func (otel *OtelConfig) Shutdown() {
+	for _, shutdown := range otel.shutdownFuncs {
+		if err := shutdown(); err != nil {
+			log.Fatalf("failed to stop exporter: %v", err)
+		}
+	}
+}
+
+func newResource(c *Config) (*resource.Resource, error) {
+	options := []resource.Option{
+		resource.WithSchemaURL(semconv.SchemaURL),
+	}
+	if c.ResourceAttributes != nil {
+		attrs := make([]attribute.KeyValue, 0, len(c.ResourceAttributes))
+		for k, v := range c.ResourceAttributes {
+			if len(v) > 0 {
+				attrs = append(attrs, attribute.String(k, v))
+			}
+		}
+		options = append(options, resource.WithAttributes(attrs...))
+	}
+	options = append(options, c.ResourceOptions...)
+	if c.ServiceName != "" {
+		options = append(options, resource.WithAttributes(semconv.ServiceNameKey.String(c.ServiceName)))
+	}
+	if c.ServiceVersion != "" {
+		options = append(options, resource.WithAttributes(semconv.ServiceVersionKey.String(c.ServiceVersion)))
+	}
+	options = append(options, resource.WithHost())
+	options = append(options, resource.WithAttributes(
+		semconv.TelemetrySDKNameKey.String("apitoolkit-otelconfig"),
+		semconv.TelemetrySDKLanguageGo,
+	))
+	options = append(options, resource.WithFromEnv())
+	return resource.New(
+		context.Background(),
+		options...,
+	)
+}
+
+func WithServiceName(name string) Option {
+	return func(c *Config) {
+		c.ServiceName = name
+	}
+}
+func WithServiceVersion(version string) Option {
+	return func(c *Config) {
+		c.ServiceVersion = version
+	}
+}
+
+func WithLogLevel(loglevel string) Option {
+	return func(c *Config) {
+		c.LogLevel = loglevel
+	}
+}
+
+func WithResourceAttributes(attributes map[string]string) Option {
+	return func(c *Config) {
+		for k, v := range attributes {
+			c.ResourceAttributes[k] = v
+		}
+	}
+}
+
+func WithResourceOption(option resource.Option) Option {
+	return func(c *Config) {
+		c.ResourceOptions = append(c.ResourceOptions, option)
+	}
+}
+
+func WithPropagators(propagators []string) Option {
+	return func(c *Config) {
+		c.Propagators = propagators
+	}
+}
+
+// Configures a global error handler to be used throughout an OpenTelemetry instrumented project.
+// See "go.opentelemetry.io/otel".
+func WithErrorHandler(handler otel.ErrorHandler) Option {
+	return func(c *Config) {
+		c.errorHandler = handler
+	}
+}
+
+func WithMetricsReportingPeriod(p time.Duration) Option {
+	return func(c *Config) {
+		c.MetricsReportingPeriod = fmt.Sprint(p)
+	}
+}
+
+func WithMetricsEnabled(enabled bool) Option {
+	return func(c *Config) {
+		c.MetricsEnabled = &enabled
+	}
+}
+
+func WithTracesEnabled(enabled bool) Option {
+	return func(c *Config) {
+		c.TracesEnabled = &enabled
+	}
+}
+
+func WithSpanProcessor(sp ...trace.SpanProcessor) Option {
+	return func(c *Config) {
+		c.SpanProcessors = append(c.SpanProcessors, sp...)
+	}
+}
+
+func WithSampler(sampler trace.Sampler) Option {
+	return func(c *Config) {
+		c.Sampler = sampler
+	}
+}
